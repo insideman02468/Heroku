d4230f51c591a1fbc944ce0422f84cffe9066990
insideman02468 <insideman02468@gmail.com>
2019-04-18 18:57:00 +0900

    add requests in requirements.txt

--- gitpush.sh
+++ gitpush.sh
@@ -4,7 +4,7 @@ git config --global user.name "insideman02468"
 git config --global user.email "insideman02468@gmail.com"
 git remote add origin https://github.com/insideman02468/Heroku.git
 git status
-git checkout initial
+git checkout
 git add --all
-git commit -m "getting start deployed"
+git commit -m "add requests in requirements.txt"
 git push origin initial
